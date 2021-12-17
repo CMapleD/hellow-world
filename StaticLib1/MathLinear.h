755d354b9edf3bbf0f681b5072c27b082036ee5d
王毅 <2929736017@qq.com>
2021-12-17 19:41:51 +0800

    Link Library

---
+++ StaticLib1/MathLinear.h
@@ -0,0 +1,12 @@
+#pragma once
+namespace Math
+{
+	class MathLinear
+	{
+	public:
+		static double Add(double a, double b);
+		static double Substrcut(double a, double b);
+		static double Mutiply(double a, double b);
+		static double Divide(double a, double b);
+	};
+}
---
+++ StaticLib1/StaticLib1.cpp
@@ -0,0 +1,25 @@
+﻿// StaticLib1.cpp : 定义静态库的函数。
+//
+
+#include "pch.h"
+#include "framework.h"
+#include "MathLinear.h"
+namespace Math
+{
+	double MathLinear::Add(double a, double b)
+	{
+		return a + b;
+	}
+	double MathLinear::Substrcut(double a, double b)
+	{
+		return a - b;
+	}
+	double MathLinear::Mutiply(double a, double b)
+	{
+		return a + b;
+	}
+	double MathLinear::Divide(double a, double b)
+	{
+		return a / b;
+	}
+}
